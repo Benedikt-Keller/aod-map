a827b6359371eed3315a089032d715589e57cfb9
51911797 <e51911797@student.tuwien.ac.at>
2023-02-07 12:06:02 +0100

    added basic sidebar

--- README 2.md
+++
@@ -1,6 +0,0 @@
-# React-Leaflet Demo
-
-Using React and Leaflet to render Markers and Popups in a map in React.
-
-- Article: Coming soon!
-- Video: Coming soon!
--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon, latLng, latLngBounds } from "leaflet";
-import * as parkData from "./data/skateboard-parks.json";
-import * as markerData from "./data/aod.json";
-import * as uniqueBuildingsJSON from "./data/aod-unique.json";
-import "./App.css";
+import { FiHome, FiChevronRight, FiSearch, FiSettings } from "react-icons/fi";
+import { divIcon, Icon, latLng, latLngBounds } from "leaflet";
 import _ from 'lodash';
 import { flushSync } from "react-dom";
+import { useState, useEffect } from "react";
 
 // setting up bounds
 
@@ -25,14 +23,71 @@ function getUnique(arr, comp) {
 return unique;
 }
 
-export default function App() { 
-  const [activePark, setActivePark] = React.useState(null);
-  const buildings = require("./data/aod.json");
-  const uniqueBuildings = getUnique(buildings.features,"Text1");
-  console.log(uniqueBuildings);
+const loadImage = (setImageDimensions, imageUrl) => {
+  const img = new Image();
+  img.src = imageUrl;
 
+  img.onload = () => {
+    setImageDimensions({
+      height: img.height,
+      width: img.width
+    });
+  };
+  img.onerror = (err) => {
+    console.log("img error");
+    console.error(err);
+  };
+};
+
+
+
+export default function App() { 
+  const [activeMarkerTitle, setActiveMarkerTitle] = React.useState(null);
+  const [activeMarkerImages, setActiveMarkerImages] = React.useState([]);
+  const [imageDimensions, setImageDimensions] = useState({});
+  const buildings = require("./data/aod-testcopy.json");
+  const uniqueBuildings = getUnique(buildings.features,"Text1");
+  const imageUrl = "";
+  let once = 1;
+  
+   // loadImage(setImageDimensions, imageUrl);
+   // console.log(imageDimensions.width, imageDimensions.height);
+
+  
+  
   return (
-    <Map center={[50.142255, 8.671575]} zoom={2.7} maxBounds={[[-90, -260],[90, 260]]} maxBoundsViscosity={1} detectRetina={true} >
+    <div style={{
+      overflow: "hidden"
+    }}> 
+    <div className="sidebar" style={{
+        fontSize: "20px",
+        fontWeight: "bold",
+        textOverflow: "ellipsis",
+        wordWrap: "break-word",
+        overflow: "scroll"
+        }}> 
+        <h1> {activeMarkerTitle} </h1>
+       
+        {activeMarkerImages.map(d => (
+        <img src={d} style={{
+          resizeMode: 'contain',
+          height: '500px'
+        }}/>
+        ))} 
+    </div>
+
+    <div>
+      {buildings.features.map(buildLinkTest => {
+        
+      })}
+    </div>
+
+    <Map center={[50.142255, 8.671575]} 
+    zoom={2.7} 
+    maxBounds={[[-90, -260],[90, 260]]} 
+    maxBoundsViscosity={1} 
+    detectRetina={true} 
+   >
       <TileLayer
         url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}"
         attribution='&copy; <Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -40,32 +95,30 @@ export default function App() {
         minZoom={3}
         maxZoom={15}
         detectRetina={true}
+        tileSize={256}
+        zoomOffset= {0}
       />
-
-      {parkData.features.map(park => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-          icon={icon}
-        />
-      ))}
 
       {uniqueBuildings.map(building => (
-        <Marker
-        key={building.Text1}
-        position={[
-          Number(building.latitude),
-          Number(building.longitude)
-        ]}
-        icon={icon}
-      />
-      ))}     
+        <Marker 
+        position = {[Number(building.latitude),Number(building.longitude)]}
+        icon = {icon}
+        key = {building.Text1}
+        onClick={() => {
+          setActiveMarkerTitle(building.Text1);
+          {const links = buildings.features.map(build => {
+            if (build.Text1 === building.Text1){
+              console.log(building.Text1, build.Image_URL)
+              return build.Image_URL
+            }})
+          setActiveMarkerImages(links)
+        }
+        }}
+        >
+        </Marker>
+      ))}
+
     </Map>
+    </div>
   );
 }
