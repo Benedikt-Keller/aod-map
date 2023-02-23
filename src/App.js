7d51d5fde05fa92e209ee78a8d04ca4b7df26947
Benedikt-Keller <benedikt.keller21@gmail.com>
2023-02-23 21:55:50 +0100

    improved pan and fixed major styling bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import { Icon, LatLng } from 'leaflet'
+import { Icon, LatLng, marker } from 'leaflet'
 import "./index.css"
 import { useState, useEffect, useRef } from "react";
 import _, { split } from 'lodash';
@@ -24,18 +24,33 @@ export const recentlySelectedIcon = new Icon({
   iconSize: [13, 13]
 });
 
-function MapController({activeLatLong, activeMarkerLat, activeMarkerLng}) {
+function MapController({ activeLatLong, centerOnMarker, setCenterOnMarker, markerClicked }) {
   const map = useMap()
-  const activeLtLng = new LatLng(Number(activeLatLong.lat) ,Number(activeLatLong.long))
-  var x = map.latLngToContainerPoint(activeLtLng).x - window.innerWidth/5.5;
+  const activeLtLng = new LatLng(Number(activeLatLong.lat), Number(activeLatLong.long))
+  var x = map.latLngToContainerPoint(activeLtLng).x - window.innerWidth / 5.5;
   var y = map.latLngToContainerPoint(activeLtLng).y;
   var point = map.containerPointToLatLng([x, y])
-  if(window.innerWidth < 1000){
-    map.panTo([Number(activeLatLong.lat) ,Number(activeLatLong.long)], {animate: true, duration: 0.5} )
-  } else {
-    map.panTo(point, {animate: true, duration: 0.5} )
-  }
+    if (window.innerWidth < 1000 || centerOnMarker) {
+      map.setMaxBounds([[-300, -260], [90, 260]])
+      map.panTo([Number(activeLatLong.lat), Number(activeLatLong.long)], {
+        animate: true,
+        duration: 0.6,
+        easeLinearity: 0.25
+      })
+    } else {
+      if(markerClicked){
+        map.setMaxBounds([[-300, -400], [90, 300]])
+      }
+      
+      map.panTo(point, { 
+        animate: true, 
+        duration: 0.6,
+        easeLinearity: 0.25
+      })
+    }
   
+  
+
   //console.log('map center:', map.getCenter())
   return null
 }
@@ -43,34 +58,39 @@ function MapController({activeLatLong, activeMarkerLat, activeMarkerLng}) {
 
 function getUnique(arr, comp) {
   // store the comparison  values in array
-    const unique =  arr.map(e => e[comp])
-  // store the indexes of the unique objects
+  const unique = arr.map(e => e[comp])
+    // store the indexes of the unique objects
     .map((e, i, final) => final.indexOf(e) === i && i)
-  // eliminate the false indexes & return unique objects
+    // eliminate the false indexes & return unique objects
     .filter((e) => arr[e]).map(e => arr[e]);
-return unique;
+  return unique;
 }
 
 
-function findFalseImages(buildings){
+function findFalseImages(buildings) {
   buildings.features.map(buildLinkTest => {
     getImageSize(buildLinkTest.Image_URL)
-    .then(({ width, height }) => {
-      if (Number(width) < 64){
+      .then(({ width, height }) => {
+        if (Number(width) < 64) {
           console.log("Wrong Image:", buildLinkTest.Image_URL)
-    }}).catch((errorMessage) => {
-          console.log(errorMessage, buildLinkTest.Text1)
-    });
+        }
+      }).catch((errorMessage) => {
+        console.log(errorMessage, buildLinkTest.Text1)
+      });
   })
 }
 
 
-export default function App() { 
+export default function App() {
+  const calcHeight = (window.innerHeight - 100).toString()
+  document.documentElement.style.setProperty('--leaflet-height', calcHeight)
   const [activeMarkerTitle, setActiveMarkerTitle] = React.useState("The Barbican, London, England");
   const [activeMarkerLat, setActiveMarkerLat] = React.useState(Number("1000"));
   const [activeMarkerLng, setActiveMarkerLng] = React.useState(Number("1000"));
   const [recentMarkerLat, setRecentMarkerLat] = React.useState(Number("1000"));
   const [recentMarkerLng, setRecentMarkerLng] = React.useState(Number("1000"));
+  const [centerOnMarker, setCenterOnMarker] = React.useState(false);
+  const [markerClicked, setMarkerClicked] = React.useState(false)
 
   const [activeMarkerImages, setActiveMarkerImages] = React.useState(
     [
@@ -85,17 +105,16 @@ export default function App() {
     ]
   );
   const [activeLatLong, setActiveLatlong] = React.useState(
-      {
-        lat: "50.142255",
-        long: "8.671575"
-      }
+    {
+      lat: "50.142255",
+      long: "8.671575"
+    }
   );
 
   const [sidebarWidth, setSidebarWidth] = React.useState("sidebar-collapsed")
-  const [leafletOffset, setLeafletOffset] = React.useState("leafet-mover")
 
   const buildings = require("./data/aod.json");
-  const uniqueBuildings = getUnique(buildings.features,"Text1");
+  const uniqueBuildings = getUnique(buildings.features, "Text1");
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -104,162 +123,176 @@ export default function App() {
       setIsLoading(true);
     }, 2000);
   }, []);
- 
+
   return (
     <>
-    {isLoading ? (
-    <div style={{ 
-      overflow: "hidden"
-    }}> 
+      {isLoading ? (
+        <div style={{
+          overflow: "hidden"
+        }}>
 
-    <MdZoomOutMap className="expand-button" onClick={() => {
+          <MdZoomOutMap className="expand-button" onClick={() => {
 
-    }}/>
+          }} />
 
-    <div className="header">
-      <span className="header-text-fullsize">ARCHITECTURE OF DOOM</span>
-      <span className="header-text-small">ARCHITECTURE OF <br></br>DOOM</span>
-      <img className="header-icon" src="/orreddot.svg"></img>
-    </div>
+          <div className="header">
+            <span className="header-text-fullsize">ARCHITECTURE OF DOOM</span>
+            <span className="header-text-small">ARCHITECTURE OF <br></br>DOOM</span>
+            <img className="header-icon" src="/orreddot.svg"></img>
+          </div>
 
-    <div className={leafletOffset}>
-    <MapContainer center={[50.142255, 8.671575]} 
-    zoom={2.7} 
-    maxBounds={[[-300, -300],[90, 260]]} 
-    maxBoundsViscosity={1} 
-    detectRetina={true} 
-    minZoom={3}
-    maxZoom={15}
-    zoomControl={false}
-    
-   >
-      <MapController 
-      activeLatLong={activeLatLong} 
-      activeMarkerLat={activeMarkerLat} 
-      activeMarkerLng={activeMarkerLng}></MapController>
-      <TileLayer
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}"
-        attribution='&copy; <Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        ext="png"
-        minZoom={3}
-        maxZoom={30}
-        detectRetina={true}
-        tileSize={256}
-        zoomOffset= {0}
-      />
-      <Marker
-      position={[activeMarkerLat,activeMarkerLng]}
-      icon={selectedIcon}
-      key={activeLatLong}
-      zIndexOffset={1000}
-      ></Marker>
 
-      <Marker
-      position={[recentMarkerLat,recentMarkerLng]}
-      icon={recentlySelectedIcon}
-      key={recentMarkerLng}
-      zIndexOffset={500}
-      eventHandlers={{
-        click: (e) => {
-          setSidebarWidth("sidebar")
-          setActiveMarkerLat(recentMarkerLat)
-            setActiveMarkerLng(recentMarkerLng)
-          document.documentElement.style.setProperty("--leaflet-offset", "25%")  
-            const sidebarDiv = document.getElementsByClassName('sidebar')[0]
-            sidebarDiv.scrollTo({
-              top: 0
-            })
-        }
-      }}
-      ></Marker>
+          <MapContainer center={[50.142255, 8.671575]}
+            zoom={2.7}
+            maxBounds={[[-300, -260], [90, 260]]}
+            maxBoundsViscosity={1}
+            detectRetina={true}
+            minZoom={3}
+            maxZoom={15}
+            zoomControl={false}
 
-      
+          >
+            <MapController
+              activeLatLong={activeLatLong}
+              centerOnMarker={centerOnMarker}
+              setCenterOnMarker={setCenterOnMarker}
+              markerClicked={markerClicked}
+            ></MapController>
 
-      {uniqueBuildings.map(building => (
-        <Marker 
-        position = {[Number(building.latitude),Number(building.longitude)]}
-        icon = {icon}
-        key = {building.Text1}
-        eventHandlers={{
-          click: (e) => {
-            // mark current marker
-            //MapContainer.setView([Number(building.latitude),Number(building.longitude)], 9)
-            console.log('marker clicked')
-            setActiveMarkerTitle(building.Text1);
-            setActiveLatlong({
-              lat: building.latitude,
-              long: building.longitude
-            })
-            const links2 = [];
-            const links = buildings.features.map(build => {
-            if (build.Text1 === building.Text1 && build.Image_URL != ""){
-              
-                const splitTags = build.Text.split("Tags:");
-                const splitNotes = build.Text.substring(14,30).split("notes");
-                const splitDesc = build.Text.split("notes")[1].split("Tags:")[0].split("View this on the map");
-                const tags = splitTags[1];
-                const date = build.Text.substring(0,14);
-                const notes = splitNotes[0];
-                const desc = splitDesc[0];
-                
+            <TileLayer
+              url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}"
+              attribution='&copy; <Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+              ext="png"
+              minZoom={2}
+              maxZoom={30}
+              detectRetina={true}
+              tileSize={256}
+              zoomOffset={0}
+            />
+            <Marker
+              position={[activeMarkerLat, activeMarkerLng]}
+              icon={selectedIcon}
+              key={activeLatLong}
+              zIndexOffset={1000}
+            ></Marker>
 
-                links2.push({
-                  link: build.Image_URL,
-                  desc: build.Text,
-                  tags: tags,
-                  date: date,
-                  notes: notes,
-                  desc: desc
-                })
-                return null
-            }})
-            setLeafletOffset("leaflet-mover-right")
-            setActiveMarkerImages(links2)
-            setSidebarWidth("sidebar")
-            setRecentMarkerLat(activeMarkerLat)
-            setRecentMarkerLat(activeMarkerLng)
-            setActiveMarkerLat(Number(building.latitude))
-            setActiveMarkerLng(Number(building.longitude))
-            document.documentElement.style.setProperty("--leaflet-offset", "translate(500px)")  
-            const sidebarDiv = document.getElementsByClassName('sidebar')[0]
-            sidebarDiv.scrollTo({
-              top: 0
-            })
-            //document.documentElement.style.setProperty("--leaflet-width", "50%")  
-            console.log(sidebarDiv)    
-          },
-        }}
-        
+            <Marker
+              position={[recentMarkerLat, recentMarkerLng]}
+              icon={recentlySelectedIcon}
+              key={recentMarkerLng}
+              zIndexOffset={500}
+              eventHandlers={{
+                click: (e) => {
+                  setMarkerClicked(true)
+                  setCenterOnMarker(false)
+                  setSidebarWidth("sidebar")
+                  setActiveMarkerLat(recentMarkerLat)
+                  setActiveMarkerLng(recentMarkerLng)
+                  const sidebarDiv = document.getElementsByClassName('sidebar')[0]
+                  try {
+                    sidebarDiv.scrollTo({
+                      top: 0
+                    })
+                  } catch (error) {
 
-        >
-        </Marker>
-        
-      ))}
+                  }
 
-    </MapContainer>
-    </div>
-    
+                }
+              }}
+            ></Marker>
 
-    <Sidebar 
-    activeMarkerTitle={activeMarkerTitle} 
-    activeMarkerImages={activeMarkerImages} 
-    sidebarWidth={sidebarWidth} 
-    setSidebarWidth={setSidebarWidth} 
-    activeLatLong={activeLatLong}
-    setActiveMarkerLat={setActiveMarkerLat}
-    setActiveMarkerLng={setActiveMarkerLng}
-    setRecentMarkerLat={setRecentMarkerLat}
-    setRecentMarkerLng={setRecentMarkerLng}
-    />
 
-    
 
-    </div>) : (
-      <div className="loading-screen">
-        <span className="loading-text"> ARCHITECTURE OF DOOM</span>
-        <img className="loading-icon" src="/orreddot.svg" ></img>
-      </div>
-    )}
+            {uniqueBuildings.map(building => (
+              <Marker
+                position={[Number(building.latitude), Number(building.longitude)]}
+                icon={icon}
+                key={building.Text1}
+                eventHandlers={{
+                  click: (e) => {
+                    // mark current marker
+                    //MapContainer.setView([Number(building.latitude),Number(building.longitude)], 9)
+                    console.log('marker clicked')
+                    setActiveMarkerTitle(building.Text1);
+                    setActiveLatlong({
+                      lat: building.latitude,
+                      long: building.longitude
+                    })
+                    const links2 = [];
+                    const links = buildings.features.map(build => {
+                      if (build.Text1 === building.Text1 && build.Image_URL != "") {
+
+                        const splitTags = build.Text.split("Tags:");
+                        const splitNotes = build.Text.substring(14, 30).split("notes");
+                        const splitDesc = build.Text.split("notes")[1].split("Tags:")[0].split("View this on the map");
+                        const tags = splitTags[1];
+                        const date = build.Text.substring(0, 14);
+                        const notes = splitNotes[0];
+                        const desc = splitDesc[0];
+
+
+                        links2.push({
+                          link: build.Image_URL,
+                          desc: build.Text,
+                          tags: tags,
+                          date: date,
+                          notes: notes,
+                          desc: desc
+                        })
+                        return null
+                      }
+                    })
+                    setMarkerClicked(true)
+                    setCenterOnMarker(false)
+                    setActiveMarkerImages(links2)
+                    setSidebarWidth("sidebar")
+                    setRecentMarkerLat(activeMarkerLat)
+                    setRecentMarkerLat(activeMarkerLng)
+                    setActiveMarkerLat(Number(building.latitude))
+                    setActiveMarkerLng(Number(building.longitude))
+                    const sidebarDiv = document.getElementsByClassName('sidebar')[0]
+                    try {
+                      sidebarDiv.scrollTo({
+                        top: 0
+                      })
+                    } catch (error) {
+
+                    }
+                  },
+                }}
+
+
+              >
+              </Marker>
+
+            ))}
+
+          </MapContainer>
+
+
+
+          <Sidebar
+            activeMarkerTitle={activeMarkerTitle}
+            activeMarkerImages={activeMarkerImages}
+            sidebarWidth={sidebarWidth}
+            setSidebarWidth={setSidebarWidth}
+            activeLatLong={activeLatLong}
+            setActiveMarkerLat={setActiveMarkerLat}
+            setActiveMarkerLng={setActiveMarkerLng}
+            setRecentMarkerLat={setRecentMarkerLat}
+            setRecentMarkerLng={setRecentMarkerLng}
+            setCenterOnMarker={setCenterOnMarker}
+            setMarkerClicked={setMarkerClicked}
+          />
+
+
+
+        </div>) : (
+        <div className="loading-screen">
+          <span className="loading-text"> ARCHITECTURE OF DOOM</span>
+          <img className="loading-icon" src="/orreddot.svg" ></img>
+        </div>
+      )}
     </>
   );
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,57 +4,59 @@ import { VscChromeClose } from "react-icons/vsc";
 import "./../index.css"
 import { RiSearch2Line } from "react-icons/ri";
 
-function setWidth(setCurrentWidth, width){
+function setWidth(setCurrentWidth, width) {
   setCurrentWidth(width)
 }
 
 
-function Sidebar({activeMarkerTitle, activeMarkerImages, sidebarWidth, setSidebarWidth, activeLatLong, setActiveMarkerLat, setActiveMarkerLng, setRecentMarkerLat, setRecentMarkerLng}){
-    const sidebarDiv = document.getElementsByClassName("sidebar")
-    return(
-    
+function Sidebar({ activeMarkerTitle, activeMarkerImages, sidebarWidth, setSidebarWidth, activeLatLong, setActiveMarkerLat, setActiveMarkerLng, setRecentMarkerLat, setRecentMarkerLng, setCenterOnMarker, setMarkerClicked }) {
+  const sidebarDiv = document.getElementsByClassName("sidebar")
+  return (
+
     <div id="sidebar" className={sidebarWidth}>
-    <div className="sidebar-header"> 
-      <div className="sidebar-header-title">
-        <div className="sidebar-heading"> 
-        {activeMarkerTitle} 
-        <VscChromeClose className="close-button" onClick={() => {
-          setSidebarWidth("sidebar-collapsed")
-          document.documentElement.style.setProperty("--leaflet-offset", "0%")
-          setRecentMarkerLat(activeLatLong.lat)
-          setRecentMarkerLng(activeLatLong.long)
-          setActiveMarkerLat(1000)
-          setActiveMarkerLng(1000)
-          const sidebarDiv = document.getElementsByClassName('sidebar')[0]
-            sidebarDiv.scrollTo({
-              top: 0,
-              behavior: "smooth"
-            })
-        }}/>
-        </div>  
+      <div className="sidebar-header">
+        <div className="sidebar-header-title">
+          <div className="sidebar-heading">
+            {activeMarkerTitle}
+            <VscChromeClose className="close-button" onClick={() => {
+              setSidebarWidth("sidebar-collapsed")
+              document.documentElement.style.setProperty("--leaflet-offset", "0%")
+              setRecentMarkerLat(activeLatLong.lat)
+              setRecentMarkerLng(activeLatLong.long)
+              setActiveMarkerLat(1000)
+              setActiveMarkerLng(1000)
+              setCenterOnMarker(true)
+              setMarkerClicked(false)
+              const sidebarDiv = document.getElementsByClassName('sidebar')[0]
+              sidebarDiv.scrollTo({
+                top: 0,
+                behavior: "smooth"
+              })
+            }} />
+          </div>
+        </div>
       </div>
-    </div>
-    
-    {activeMarkerImages.map(element => (
-      <>
-      <div className="content-container">
-        <img src={element.link} className="img-style" alt=""></img>
-        <p className="basic-text"> 
-        <b> date: </b>{element.date} <br/>
-        <b> tags: </b>{element.tags} <br/>
-        <b> notes: </b>{element.notes} <br/>
-        <b> desc: </b>{element.desc} <br/>
-        <span className="sidebar-space">‎</span> 
-        </p>
-      </div>
-      
-      </>
-    ))
-    }
+
+      {activeMarkerImages.map(element => (
+        <>
+          <div className="content-container">
+            <img src={element.link} className="img-style" alt=""></img>
+            <p className="basic-text">
+              <b> date: </b>{element.date} <br />
+              <b> tags: </b>{element.tags} <br />
+              <b> notes: </b>{element.notes} <br />
+              <b> desc: </b>{element.desc} <br />
+              <span className="sidebar-space">‎</span>
+            </p>
+          </div>
+
+        </>
+      ))
+      }
     </div>
 
-    )
-    
+  )
+
 }
 
 export default Sidebar;
